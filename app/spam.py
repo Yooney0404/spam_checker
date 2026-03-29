4a8de4e02fec68ed4de56aec174692782a6fad88
Yooney0404 <sptldi0404@gmail.com>
2026-03-29 19:33:22 +0900

    test1_snd_commit

--- app/spam.py
+++ app/spam.py
@@ -6,7 +6,7 @@ def check_spam(text: str) -> tuple:
     # 스팸 키워드 목록
     spam_keywords = [
         "free", "win", "winner", "prize", "click", 
-        "buy now", "urgent", "cash", "money", "offer", "deal"
+        "buy now", "urgent", "cash", "money", "offer", "deal", "now"
     ]
     
     hit = 0
